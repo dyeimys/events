231501783b9940c3774e47b25f4c98034fd13927
Dyeimys Franco <dyeimys@outlook.com>
2023-03-19 18:52:31 -0300

    WIP - Reafactor models

--- src/main/kotlin/br/dfranco/learn/events/web/request/EventRequest.kt
+++ src/main/kotlin/br/dfranco/learn/events/web/request/EventRequest.kt
@@ -5,9 +5,11 @@ import java.time.LocalDateTime
 import java.util.*
 
 data class EventRequest(
-        @Schema(example = "My Event")
+        @Schema(example = "My Event", description = "Name of event")
         var name: String,
+        @Schema(description = "Date of event")
         var date: LocalDateTime,
+        @Schema(description = "Location of event")
         var locationId: UUID?,
         @Schema(example = "Mr. James")
         var owner: String,
